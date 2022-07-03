4daea7cabbf0f0793cc4ec26afc9b8130cb5b70c
samlessheart <cyrilekta@gmail.com>
2022-07-03 11:42:25 +0530

    debug db

--- members/admin.py
+++ members/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import MyUser, Profile
+from .models import MyUser, #Profile
 from django.contrib.auth.models import Group
 # Register your models here.
 
 admin.site.register(MyUser)
-admin.site.register(Profile)
+#admin.site.register(Profile)
 
 admin.site.unregister(Group)
 
